18057de5823150b74e3f34167b36a68b0b976af5
Alvaro Separovich <alvasepro@gmail.com>
2019-07-19 13:33:09 -0300

    Start to split in many archives

---
+++ includes/front.js
@@ -0,0 +1,169 @@
+function Front(){
+	this.bd = document.querySelector("body");
+
+	this.on_pdv_price = function(price, salePrice){
+		arrayElements = this.price_to_HTML_elements(price, salePrice, true);
+
+		arrayElements.percentageElement.style	 = "color: white;background: green;padding: 2px 6px;display: inline;right: 0;font-size: 3em;";
+		arrayElements.salePriceElement.style	 = "font-size: 1.5em;";
+		arrayElements.priceElement.style		 = "font-size: 2em;";
+
+		div 		= document.createElement("div");
+		div.style 	= "display: inline-block;font-size: 1em;position: absolute;right: 100px;";
+		div.appendChild( arrayElements.salePriceElement );
+		div.appendChild( arrayElements.priceElement );
+
+		priceElement 		= document.createElement("div");
+		priceElement.style	= "display: inline; position: absolute; bottom: 0; right: 0;";
+		priceElement.id 	= "price_element";
+		
+		priceElement.appendChild( div );
+		priceElement.appendChild( arrayElements.percentageElement );
+
+		detalhes_produto = this.bd.querySelector("#detalhes_produto");
+		if(detalhes_produto.querySelector("#price_element")){
+			detalhes_produto.removeChild(detalhes_produto.querySelector("#price_element"))
+		}
+		detalhes_produto.appendChild(priceElement);
+	}
+	this.price_to_HTML_elements = function(price, salePrice, returnArray = false){
+		divPriceElement = document.createElement("div"); 
+		
+		salePriceElement = document.createElement("del");
+		salePriceElement.innerText = "R$" + price;
+		
+		priceElement = document.createElement("h2");
+		priceElement.innerText = "R$" + salePrice;
+
+		percentageElement = document.createElement("h1");
+		salePriceFloat = parseFloat(salePrice.replace('.','').replace(',','.'));
+		priceFloat = parseFloat(price.replace('.','').replace(',','.'));
+		percentageElement.innerText = "%" + ( 100 - salePriceFloat*100/priceFloat ).toFixed();
+		percentageElement.style = "color: white;background: green;padding: 2px 6px;display: inline;right: 0;margin-top: -40px;position: fixed;font-size: 3em;";
+
+		divPriceElement.appendChild(salePriceElement);
+		divPriceElement.appendChild(priceElement);
+		divPriceElement.appendChild(percentageElement);
+
+		divPriceElement.style = "order:3;";
+		
+		if(returnArray){
+			return { 'salePriceElement': salePriceElement, 'priceElement': priceElement, 'percentageElement' : percentageElement, 'divPriceElement' : divPriceElement };
+		}else{
+			return divPriceElement;
+		}
+	}
+	this.on_pdv_estoque = function(DOMObj){
+		
+		DOMObj.style = "position:absolute;right:0;width:auto;top:20px;";
+		DOMObj.classList.add("without-green");
+		detalhes_produto = this.bd.querySelector("#detalhes_produto");
+		if(detalhes_produto.querySelector(".datatable")){
+			detalhes_produto.removeChild(detalhes_produto.querySelector(".datatable"))
+		}
+		detalhes_produto.appendChild(DOMObj);
+	}
+	this.on_pdv_editora = function(nomeEditora){
+		console.log(nomeEditora);
+		h2 = document.createElement("h2");
+		h2.id = "editora_da_livro";
+		h2.innerHTML = '<span style="background: gray; color: white; padding: 0.3em;">{0}</span>'.replace('{0}',nomeEditora);
+
+		document.view.delete_editora_pdv();
+
+		detalhes_produto = document.body.querySelector("#detalhes_produto");
+		detalhes_produto.appendChild(h2);
+	}
+	this.pop_img_estoque = function(element){
+		
+		if (typeof element == "string"){
+			img = document.createElement("img");
+			img.src = element
+		}else{
+			img = element;
+		}
+		img.style = "width:20vh;max-width: 100%;order:0;"
+		this.append_to_pop(img);
+	}
+	this.pop_title_product = function(title){
+		element = document.createElement("h2");
+		element.style = "order:1;font-size: 1.2em;";
+		element.innerText = title;
+
+		this.append_to_pop(element);
+	}
+	this.pop_isbn_product = function(isbn){
+		element = document.createElement("h3");
+		element.style = "order:2;margin: 0;color: #3faf6c;";
+		element.innerText = isbn;
+
+		this.append_to_pop(element);
+	}
+	this.pop_price_product = function(price, salePrice){
+
+		pricesElement = this.price_to_HTML_elements(price, salePrice);
+		this.append_to_pop(pricesElement);
+	}
+	this.pop_stock_product = function(DOMObj){
+		DOMObj.style = "order:10;"
+		this.append_to_pop(DOMObj);
+	}
+	this.pop_editora_product = function(nomeEditora){
+		deleteBtn = document.querySelector("#delete_button");
+		deleteBtn.innerHTML ='<h2 style="color: white; text-align:center;">{0}</h2>'.replace('{0}',nomeEditora) ;
+	}
+	this.show_qtd_on_pdv = function(n){
+		qtd = document.querySelector("#button_qtd>#qtd");
+		qtd.innerHTML = n;
+	}
+	this.popup = function(isbn = '#'){
+
+		if ( !this.delete_popup(isbn) ){
+			return false
+		};
+
+		popup = document.createElement("div");
+		popup.id = "pop_info";
+		popup.style = "position:fixed;top:0;right:0;z-index:41;width:250px;background:#f2f2f2;padding:10px;margin-top:44px;display:flex;flex-direction:column;border: solid #3faf6c;";
+
+		delete_button = document.createElement("div");
+		delete_button.addEventListener("click" , this.delete_popup );
+		delete_button.id = "delete_button";
+		delete_button.style = "height: 25px;margin:-10px -10px 4px;background:gray;order:-1;";
+		popup.appendChild(delete_button);
+
+		this.bd.appendChild(popup);
+	}
+	this.append_to_pop = function( element ){
+		popup = this.bd.querySelector("#pop_info");
+		popup.appendChild(element);
+	}
+	this.delete_popup = function(isbn = '#'){
+
+
+		popup = document.querySelector("body").querySelector("#pop_info");
+		if(popup){
+			if( popup.querySelector("h3").innerText != isbn){
+				popup.parentElement.removeChild(popup);
+				return true;
+			}else{
+				return false;
+			}
+		}
+		return true;
+	}
+	this.delete_editora_pdv = function(isbn = '#'){
+
+		editora_on_pdv = document.querySelector("#detalhes_produto").querySelector("#editora_da_livro");
+		if(editora_on_pdv){
+			editora_on_pdv.parentElement.removeChild(editora_on_pdv);
+		}
+	}
+	this.product_green_space_to_click = function(){
+		if(/(.*Produtos - Bling.*)/.test( document.title )){
+			style = document.createElement("style");
+			style.innerHTML = "@media screen and (min-width:992px){.imagem_produto{max-height:350px!important}.box-content #datatable td:first-child,.box-content #datatable th:first-child,.box-content .datatable td:first-child,.box-content .datatable th:first-child{padding-left:40px;background:rgba(63,175,108,1);background:linear-gradient(to right,rgba(63,175,108,1) 0,rgba(63,175,108,1) 47%,rgba(63,175,108,0) 47%,rgba(63,175,108,0) 48%,rgba(255,255,255,0) 49%,rgba(255,255,255,0) 100%)}}"
+			document.head.appendChild(style);
+		}
+	}
+} front = new Front(); document.view = new Front();
---
+++ includes/sFetch.js
@@ -0,0 +1,154 @@
+function sFetch(id = 0, type = 0, handle_response = 0) {
+	this.response = "";
+	this.parsedXML = "";
+	this.DOMObj = "";
+
+	this.fetch = function(url,headers, handle_response = 0){
+		fetch(url,headers)
+		.then( response => response.text() )
+		.then( data => { 
+			this.response = data; 
+			if(handle_response){
+				handle_response(data);
+			}else{
+				console.log(data);
+			}
+		} )
+		//.then( strResponse => {this.parsedXML = this.strToXML(strResponse);})
+	};
+	this.xmlToJson= function( xml = this.parsedXML ) {// Unminifyed code on https://gist.github.com/chinchang/8106a82c56ad007e27b1
+		var obj={};
+		if(1==xml.nodeType){if(xml.attributes.length>0){obj["@attributes"]={};for(var j=0;j<xml.attributes.length;j++){var attribute=xml.attributes.item(j);obj["@attributes"][attribute.nodeName]=attribute.nodeValue}}}else 3==xml.nodeType&&(obj=xml.nodeValue);if(xml.hasChildNodes()&&1===xml.childNodes.length&&3===xml.childNodes[0].nodeType)obj=xml.childNodes[0].nodeValue;else if(xml.hasChildNodes())for(var i=0;i<xml.childNodes.length;i++){var item=xml.childNodes.item(i),nodeName=item.nodeName;if(void 0===obj[nodeName])obj[nodeName]=xmlToJson(item);else{if(void 0===obj[nodeName].push){var old=obj[nodeName];obj[nodeName]=[],obj[nodeName].push(old)}obj[nodeName].push(xmlToJson(item))}} return obj;
+	};
+	this.strToXML = function( stringToParse = this.response ){
+		if (window.DOMParser) {/*code for modern browsers*/	
+			parser = new DOMParser();return parser.parseFromString( stringToParse , "text/xml" );
+		} else { /*code for old IE browsers*/ 
+			xmlDoc = new ActiveXObject("Microsoft.XMLDOM");xmlDoc.async = false;return xmlDoc.loadXML(text);
+		} 
+	};
+	this.strToHTML = function ( htmlString = this.response ) {
+	  var div = document.createElement('div'); 
+	  div.innerHTML = htmlString.trim();
+	  this.DOMObj = div; 
+	  return div; 
+	};
+	this.processHTMLStoque = function(parsedXML = this.parsedXML){
+		//@ parsedXML = XML
+		//Return -> HTML
+
+		innerHTMLstr = parsedXML.documentElement.querySelector("[t='totalEstoque']").innerHTML;
+		numberStartersCharacteres = 9;
+		subHTMLStr = innerHTMLstr.substr(numberStartersCharacteres, innerHTMLstr.length - (numberStartersCharacteres + 3));
+		div = this.strToHTML(subHTMLStr);
+		divTable = div.querySelector("table.datatable")
+		this.DOMObj = divTable;
+		return divTable;
+	};
+	this.getCookie = function(cname="WSSID") {//code of https://www.w3schools.com/js/js_cookies.asp
+	  var name = cname + "=";
+	  var decodedCookie = decodeURIComponent(document.cookie);
+	  var ca = decodedCookie.split(';');
+	  for(var i = 0; i <ca.length; i++) {
+	  	var c = ca[i];
+	  	while (c.charAt(0) == ' ') {c = c.substring(1);}
+	  	if (c.indexOf(name) == 0) {
+	  		return c.substring(name.length, c.length);
+	  	}
+	  }return "";
+	};
+	this.getStockById = function( productID = "2817352204" , handle_response = 0){
+		var cookie = this.getCookie();
+		header = {
+			"credentials":"include",
+			"headers":{
+				"accept":"*/*",
+				"accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+				"content-type":"application/x-www-form-urlencoded",
+				"session-token":cookie
+			},
+			"referrer":"https://www.bling.com.br/estoque.php?",
+			"referrerPolicy":"no-referrer-when-downgrade",
+			"body":"xajax=listarLancamentos&xajaxs="+cookie+"&xajaxr=1560859712223&xajaxargs[]=ultimos&xajaxargs[]="+productID+"&xajaxargs[]=0&xajaxargs[]=1&xajaxargs[]=&xajaxargs[]=&xajaxargs[]=5888049670&xajaxargs[]=",
+			"method":"POST",
+			"mode":"cors"
+		};
+		if(handle_response){
+			this.fetch("https://www.bling.com.br/services/estoques.server.php?f=listarLancamentos", header , handle_response);
+		}else{
+			return this.fetch("https://www.bling.com.br/services/estoques.server.php?f=listarLancamentos", header );
+		}
+	};
+	this.getImgById = function( productID = "2817352204" , handle_response = 0){
+		var cookie = ""+this.getCookie();
+		header = {"credentials":"include",
+			"headers":{
+				"accept":"*/*",
+				"accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+				"content-type":"application/x-www-form-urlencoded",
+				"session-token":cookie
+			},
+				"referrer":"https://www.bling.com.br/pdv.php",
+				"referrerPolicy":"no-referrer-when-downgrade",
+				"body":"xajax=obterProduto&xajaxs="+cookie+"&xajaxr=1560444290700&xajaxargs[]=%3Cxjxobj%3E%3Ce%3E%3Ck%3Eid%3C%2Fk%3E%3Cv%3E"+productID+"%3C%2Fv%3E%3C%2Fe%3E%3Ce%3E%3Ck%3EidLoja%3C%2Fk%3E%3Cv%3E203331767%3C%2Fv%3E%3C%2Fe%3E%3Ce%3E%3Ck%3EidVenda%3C%2Fk%3E%3Cv%3E0%3C%2Fv%3E%3C%2Fe%3E%3Ce%3E%3Ck%3EidDeposito%3C%2Fk%3E%3Cv%3E5888049670%3C%2Fv%3E%3C%2Fe%3E%3C%2Fxjxobj%3E",
+				"method":"POST",
+			"mode":"cors"};
+		if(handle_response){
+			this.fetch("https://www.bling.com.br/services/frentes.caixas.server.php?f=obterProduto", header, handle_response);
+		}else{
+			this.fetch("https://www.bling.com.br/services/frentes.caixas.server.php?f=obterProduto", header);
+		}
+	};
+	this.getMultilojaPrice = function( productID = "2817352204" , handle_response = 0){
+		var cookie = ""+this.getCookie();
+		//console.log("Get Multiloja Price");
+		header = {"credentials":"include",
+			"headers":{
+				"accept":"*/*",
+				"accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+				"content-type":"application/x-www-form-urlencoded",
+				"session-token":cookie
+			},
+			"referrer":"https://www.bling.com.br/produtos.php",
+			"referrerPolicy":"no-referrer-when-downgrade",
+			"body":"xajax=obterVinculoProdutosMultilojas&xajaxs=" + cookie + "&xajaxr=1562091621113&xajaxargs[]=" + productID,
+			"method":"POST",
+			"mode":"cors"
+		};
+		if(handle_response){
+			this.fetch("https://www.bling.com.br/services/produtos.server.php?f=obterVinculoProdutosMultilojas", header , handle_response);
+		}else{
+			return this.fetch("https://www.bling.com.br/services/produtos.server.php?f=obterVinculoProdutosMultilojas", header );
+		}
+	};
+	this.getEditora = function( productID = "2817352204" , handle_response = 0){
+		var cookie = ""+this.getCookie();
+		header = {
+			"credentials":"include",
+			"headers":{
+				"accept":"*/*",
+				"accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+				"content-type":"application/x-www-form-urlencoded",
+				"session-token":cookie
+			},
+			"referrer":"https://www.bling.com.br/produtos.php",
+			"referrerPolicy":"no-referrer-when-downgrade",
+			"body":"xajax=obterProduto&xajaxs=" + cookie + "&xajaxr=1563218566001&xajaxargs[]=" + productID,
+			"method":"POST",
+			"mode":"cors"
+		};
+		if(handle_response){
+			this.fetch("https://www.bling.com.br/services/produtos.server.php?f=obterProduto", header , handle_response);
+		}else{
+			return this.fetch("https://www.bling.com.br/services/produtos.server.php?f=obterProduto", header );
+		}
+	};
+
+	if(id){
+		if(type == "img"){
+			return this.getImgById(id);
+		}else if( type == "stock"){
+			this.getStockById(id);
+		}
+	};
+} document.sF = new sFetch();
