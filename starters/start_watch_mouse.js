def196818309b70b943775ace80ae8ccf8ef49d5
Alvaro Separovich <alvasepro@gmail.com>
2019-07-19 16:30:29 -0300

    Fix: dependecy access

--- starters/start_watch_mouse.js
+++ starters/start_watch_mouse.js
@@ -2,7 +2,7 @@ document.onclick = function(e){
 	pop = document.querySelector("#pop_info");
 	if(e.target != pop && e.target.parentElement != pop && e.target.parentElement.parentElement.parentElement.parentElement.parentElement == document.querySelector("#datatable")){
 		console.log("This is the element");
-		back.process_clicked_item(e.target.parentElement);
+		document.back.process_clicked_item(e.target.parentElement);
 	}else if(pop){
 		pop.parentElement.removeChild(pop);
 	}
