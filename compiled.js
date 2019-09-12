ebdf816a927e6bc11be6ffbfacec473a26177bc2
Alvaro Separovich <alvasepro@gmail.com>
2019-09-12 15:20:10 -0300

    -Fix: Limit PopUp trigger

--- compiled.js
+++ compiled.js
@@ -631,7 +631,7 @@ function sFetch(id = 0, type = 0, handle_response = 0) {
 
 document.onclick = function(e){
 	pop = document.querySelector("#pop_info");
-	if(e.target != pop && e.target.parentElement != pop && e.target.parentElement.parentElement.parentElement.parentElement.parentElement == document.querySelector("#datatable")){
+	if( /(.*bling\.com\.br\/produtos\.php.*)/.test(document.URL)  && e.target != pop && e.target.parentElement != pop && e.target.parentElement.parentElement.parentElement.parentElement.parentElement == document.querySelector("#datatable")){
 		console.log("This is the element");
 		document.back.process_clicked_item(e.target.parentElement);
 	}else if(pop){
